fefd96e6e2e6a7db7ef8a240a3e01bd0becf34f4
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-10-28 15:18:51 +0100

    Wow!

--- RavenDB/SampleRavenDBApp/SampleRavenDBApp/Program.cs
+++ RavenDB/SampleRavenDBApp/SampleRavenDBApp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Raven.Client.Document;
+using Raven.Imports.Newtonsoft.Json;
 
 namespace SampleRavenDBApp
 {
@@ -10,6 +12,27 @@ namespace SampleRavenDBApp
 	{
 		static void Main(string[] args)
 		{
+			var store = new DocumentStore()
+			{
+				Url = "localhost",
+				DefaultDatabase = "Northwind"
+			};
+			store.Initialize();
+			using (var session = store.OpenSession())
+			{
+				var p = session.Load<Product>("Product/1");
+				p.Description = "descr";
+				session.SaveChanges();
+			}
 		}
+	}
+	public class Product
+	{
+
+		//[JsonProperty(PropertyName = "bla")]
+		public string Id { get; private set; }
+		public string Description { get; set; }
+		public string Name { get; set; }
+		public decimal Price { get; set; }
 	}
 }
